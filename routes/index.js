47cdc47f63ceea75292ebe072b9d484735838a2d
arpitnakranii <141709160+arpitnakranii@users.noreply.github.com>
2024-10-01 12:17:45 +0530

    update

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 var {register, login}=require('../controller/user')
-
+router.get('/',function (req,res){
+  res.send("Hello")
+  res.end()
+})
 router.post('/user/register', register);
 router.post('/user/login',login)
 module.exports = router;
